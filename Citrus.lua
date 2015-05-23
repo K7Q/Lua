238f90c27b0cf04a2e5cde0d6c6f5a78ca84589e
ParadoxTheorem <davidrgarland@me.com>
2015-05-23 12:17:30 -0500

    Citrus.lua
    Citrus is a lua module to add simple 3D manipulation and equations to lua for easy use.

---
+++ Citrus.lua
@@ -0,0 +1,15 @@
+------------------------
+-- Citrus Lua Module ---
+-- Epoch Studios 2015 --
+-- Work In Progress ----
+------------------------
+
+Citrus = {}
+
+Citrus.tr = function(sx,sy,sz,ex,ey,ez,mode,val)
+   if mode == "time" then
+      
+   end
+end
+
+return Citrus
